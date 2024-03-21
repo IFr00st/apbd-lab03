4c018df8466d30d1ca111f5c0be92908f023e142
s27885 <s27885@pjwstk.edu.pl>
2024-03-21 15:00:05 +0100

    Add container class and OverfillException

--- lab03/lab03/Containers/Container.cs
+++ lab03/lab03/Containers/Container.cs
@@ -1,26 +1,45 @@
-﻿namespace lab03.Containers;
+﻿using lab03.Exceptions;
+
+namespace lab03.Containers;
 using lab03.Interfaces;
 
 
 public class Container : IContainer
 {
     private double _cargoWeight;
+    private double _cargoheight;
+    private double _containerWeight;
+    private double _containerDepth;
+    protected static int id = 0;
+    protected string serialNumber;
+    protected int _containerid;
+    private double _maxload;
 
 
-    public Container(double cargoWeight)
+    public Container(double cargoheight, double containerWeight, double containerDepth, double maxload, double cargoWeight)
     {
         _cargoWeight = cargoWeight;
+        _cargoheight = cargoheight;
+        _containerWeight = containerWeight;
+        _containerDepth = containerDepth;
+        _maxload = maxload;
+        CargoWeight = cargoWeight;
+        _containerid = id++;
+        serialNumber = "CON-C-" + _containerid;
     }
 
 
     public double CargoWeight { get; set; }
     public void Unload()
     {
-        throw new NotImplementedException();
+        _cargoWeight = 0;
     }
 
     public virtual void Load(double cargoWeight)
     {
-        throw new NotImplementedException();
+        if (_cargoWeight + cargoWeight > _maxload)
+        {
+            throw new OverfillException("Too big cargo");
+        }
     }
 }
